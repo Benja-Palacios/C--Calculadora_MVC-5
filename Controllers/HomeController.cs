5cb9cfda91966f80465875745a8c824e5966afa2
Benjamin Mendoza <benjijr96@gmail.com>
2023-02-08 00:02:05 -0600

    Upload first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using EvaluacionCalculadora.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace EvaluacionCalculadora.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Mensaje = "Resultados de las Operaciones Seleccionadas";
+
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Sumatoria = TempData["suma"];
+            ViewBag.Restatoria = TempData["resta"];
+            ViewBag.Multitoria = TempData["multi"];
+            ViewBag.Diviciontoria = TempData["divicion"];
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Calculadora() {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Calculadora(OperacionesBasicas operacionesBasicas)
+        {
+            if (operacionesBasicas.opSuma == true)
+            {
+                TempData["suma"] = operacionesBasicas.num1 + operacionesBasicas.num2;
+            }
+            if (operacionesBasicas.opResta == true)
+            {
+                TempData["resta"] = operacionesBasicas.num1 - operacionesBasicas.num2;
+            }
+            if (operacionesBasicas.opMulti == true)
+            {
+                TempData["multi"] = operacionesBasicas.num1 * operacionesBasicas.num2;
+            }
+            if (operacionesBasicas.opDivicion == true)
+            {
+                TempData["divicion"] = operacionesBasicas.num1 / operacionesBasicas.num2;
+            }
+
+            return RedirectToAction("About", "Home");
+        }
+    }
+}
---
+++ ViewModel/OperacionesBasicas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvaluacionCalculadora.ViewModel
+{
+    public class OperacionesBasicas
+    {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+        public bool opSuma { get; set; }
+        public bool opResta { get; set; }
+        public bool opMulti { get; set; }
+        public bool opDivicion { get; set; }
+    }
+}
